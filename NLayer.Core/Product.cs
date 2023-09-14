b622ecd030957b3eb0ac936f61d9c8a3ca8b10bf
Nur Yavuz <nuryavuzz@outlook.com>
2023-09-14 13:59:40 +0300

    Entity Configurations

--- NLayer.Core/Product.cs
+++ NLayer.Core/Product.cs
@@ -13,6 +13,6 @@ namespace NLayer.Core
         public decimal Price { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
-        public ProductFeature productFeature { get; set; }
+        public ProductFeature ProductFeature { get; set; }
     }
 }
--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NLayer.Core;
+using NLayer.Repository.Configurations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,5 +20,13 @@ namespace NLayer.Repository
         public DbSet<Category>Categories { get; set; }
         public DbSet<Product>Products { get; set; }
         public DbSet<ProductFeature>ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
