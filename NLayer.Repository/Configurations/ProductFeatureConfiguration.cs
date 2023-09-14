40611d5e1ee523fe858953e0f73a8128551f06db
Nur Yavuz <nuryavuzz@outlook.com>
2023-09-14 14:14:57 +0300

    Entity Configurations

--- NLayer.Repository/Configurations/ProductFeatureConfiguration.cs
+++ NLayer.Repository/Configurations/ProductFeatureConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,13 @@ using System.Threading.Tasks;
 
 namespace NLayer.Repository.Configurations
 {
-    internal class ProductFeatureConfiguration
+    internal class ProductFeatureConfiguration : IEntityTypeConfiguration<ProductFeature>
     {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.HasOne(x => x.Product).WithOne(x => x.ProductFeature).HasForeignKey<ProductFeature>(x => x.ProductId);
+        }
     }
 }
